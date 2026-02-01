ce2d4bacecb4d6a4c93ec1dbb7b9e74a0c4ea7cf
JalaluddinYazdani <jalal.enggds@gmail.com>
2026-02-02 00:44:36 +0530

    Code with Parsing the Json Data

---
+++ MVNAPIFramework/src/test/java/com/org/ParseResBody.java
@@ -0,0 +1,40 @@
+package com.org;
+import io.restassured.response.Response;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.restassured.matcher.ResponseAwareMatcher;
+import io.restassured.*;
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matcher.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class ParseResBody{
+
+ @Test
+ public void getStudentsData(){
+     POJOClass pojo= new POJOClass();;
+     Response res= given().contentType("application/json")
+             .when().get("http://localhost:3000/students");
+
+     JSONArray ja= new JSONArray(res.asString());
+     for(int i=0;i<ja.length();i++){
+         System.out.println(ja.getJSONObject(i).get("name").toString());
+     }
+ }
+
+    @Test
+    public void getBooksData(){
+        Response res= given()
+                        .when().get("http://localhost:3000/books");
+        JSONArray ja= new JSONArray(res.asString());
+
+        for(int i=0;i<ja.length();i++) {
+            System.out.println(ja.getJSONObject(i).get("title").toString());
+
+        }
+    }
+
+}
